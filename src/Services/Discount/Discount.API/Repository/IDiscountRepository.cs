af1919b333019eea176306a59ab74426b33c64ab
karasakal <hmkrskl@gmail.com>
2022-03-13 12:45:50 +0300

    discount api repository pattern

---
+++ src/Services/Discount/Discount.API/Repository/IDiscountRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Discount.API.Entities;
+
+namespace Discount.API.Repository {
+    public interface IDiscountRepository {
+        Task<Coupon> GetDiscount(string productName);
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(string productName);
+    }
+}
