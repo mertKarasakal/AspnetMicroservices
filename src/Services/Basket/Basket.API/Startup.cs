ae9811e14c5c86410c57d312f2c0885a4fb4000f
karasakal <hmkrskl@gmail.com>
2022-03-14 22:46:33 +0300

    register grpc client

--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -1,4 +1,7 @@
+using System;
+using Basket.API.GrpcServices;
 using Basket.API.Repositories;
+using Discount.Grpc.Protos;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -16,15 +19,17 @@ namespace Basket.API {
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
-          
+
             //IDistributedCache Dependency Injection
-            services.AddStackExchangeRedisCache(options =>
-            {
-                options.Configuration = 
+            services.AddStackExchangeRedisCache(options => {
+                options.Configuration =
                     Configuration.GetValue<string>("CacheSettings:ConnectionString");
             });
 
             services.AddScoped<IBasketRepository, BasketRepository>();
+            services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(o =>
+                o.Address = new Uri(Configuration["GrpcSettings:DiscountUrl"]));
+            services.AddScoped<DiscountGrpcService>();
 
             services.AddControllers();
             services.AddSwaggerGen(c => {
