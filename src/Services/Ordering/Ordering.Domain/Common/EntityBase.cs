6fdf04f99c0a558cb2de0ab6c36e3c061c29e79e
karasakal <hmkrskl@gmail.com>
2022-03-15 18:57:21 +0300

    ordering.domain entities

---
+++ src/Services/Ordering/Ordering.Domain/Common/EntityBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Ordering.Domain.Common {
+    public abstract class EntityBase {
+        public int Id { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
