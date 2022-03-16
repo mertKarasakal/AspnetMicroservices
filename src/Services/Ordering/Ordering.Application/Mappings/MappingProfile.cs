bed1e0ff1eae5e2584e8c20bd699cdd39b47bef1
karasakal <hmkrskl@gmail.com>
2022-03-16 19:20:22 +0300

    CheckoutOrderCommand

--- src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
+++ src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
@@ -1,12 +1,13 @@
 ﻿using AutoMapper;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
 using Ordering.Application.Features.Orders.Queries.GetOrdersList;
 using Ordering.Domain.Entities;
 
 namespace Ordering.Application.Mappings {
     public class MappingProfile : Profile {
-        public MappingProfile()
-        {
+        public MappingProfile() {
             CreateMap<Order, OrdersVm>().ReverseMap();
+            CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
         }
     }
 }
