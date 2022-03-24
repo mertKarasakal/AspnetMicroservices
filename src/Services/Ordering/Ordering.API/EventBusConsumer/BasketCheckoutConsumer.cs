afaacc002c34156db89842fdb75a7e80e4dafafc
karasakal <hmkrskl@gmail.com>
2022-03-24 19:16:21 +0300

    basket checkout consumer

---
+++ src/Services/Ordering/Ordering.API/EventBusConsumer/BasketCheckoutConsumer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using EventBus.Messages.Events;
+using MassTransit;
+namespace Ordering.API.EventBusConsumer {
+    public class BasketCheckoutConsumer : IConsumer<BasketCheckoutEvent> {
+        public Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
