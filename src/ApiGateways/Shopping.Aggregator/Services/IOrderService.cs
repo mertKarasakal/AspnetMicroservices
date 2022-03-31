1d3b4850176a2828ac902b895ad69779ff86c486
karasakal <hmkrskl@gmail.com>
2022-03-31 18:10:49 +0300

    shopping aggregation service classes

---
+++ src/ApiGateways/Shopping.Aggregator/Services/IOrderService.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Shopping.Aggregator.Models;
+namespace Shopping.Aggregator.Services {
+    public interface IOrderService {
+        Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName);
+    }
+}
